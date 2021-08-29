ac01a1c904b6c57e5deb9b71e0d6ebb806089ba3
Evgenii Shein <eshein@neoflex.ru>
2021-08-29 18:42:28 +0400

    add comp list

--- src/main/java/pack/Bot.java
+++ src/main/java/pack/Bot.java
@@ -8,13 +8,17 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-public class Bot extends TelegramLongPollingBot {
+import java.util.Random;
+
+public class Bot extends TelegramLongPollingBot implements Compliments {
+
+    Random rnd = new Random();
+    int len = list.length;
 
     Bot(){
 //        System.getProperties().put( "proxySet", "true" );
 //        System.getProperties().put( "socksProxyHost", "127.0.0.1" );
 //        System.getProperties().put( "socksProxyPort", "9150" );
-
         System.out.println("dll OK");
     }
 
@@ -36,10 +40,15 @@ public class Bot extends TelegramLongPollingBot {
         sendMessage.setReplyToMessageId(message.getMessageId());
         sendMessage.setText(text);
         try {
+            System.out.println(text);
             execute(sendMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+    }
+
+    private String generateCompliment() {
+       return "Ты такая " + list[rnd.nextInt(len)].toLowerCase() + "! :-)";
     }
 
     @Override
@@ -48,7 +57,7 @@ public class Bot extends TelegramLongPollingBot {
         if (message != null && message.hasText()) {
             String s = message.getText();
             System.out.println(s);
-            sendMsg(message, "Привет " + s);
+            sendMsg(message, generateCompliment());
         }
     }
 
